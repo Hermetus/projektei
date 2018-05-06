64269439f7a961a95f01308b89cc5110fb260d53
Heta S. Seinä <heta.seina@gmail.com>
2018-05-06 19:30:39 +0300

    Kolmas tehtävä ratkaistu.

--- projecte.cpp
+++ projecte.cpp
@@ -2,8 +2,8 @@
 // ProjectEuler tehtävien ratkaisuyritelmiä
 // Aloitettu 4.5.2018
 
-#include "stdafx.h"
 #include <iostream>
+#include <cmath>
 
 void problem1();
 void problem2();
@@ -53,28 +53,31 @@ void problem2() {
 void problem3() {
 	//Problem 3.
 	//Largest prime factor of a number
-	int n = 600851475143;
-	int largestPrimeFactor = 1;
+	long long int n = 600851475143;
+	long long int largestPrimeFactor = 1;
+
 	if (n % 2 == 0) {
 		largestPrimeFactor = 2;
 		while (n % 2 == 0) {
 			n = n / 2;
 		}
 	}
-	if (n % 3 == 0) {
-		largestPrimeFactor = 3;
-		while (n % 3 == 0) {
-			n = n / 3;
+
+	long long int possFactor = 3;
+	while (possFactor < sqrt(n)) {
+		if (n % possFactor == 0) {
+			largestPrimeFactor = possFactor;
 		}
-	}
-	int multOfSix = 6;
-	while (multOfSix - 1 <= n) {
-		if (n % multOfSix - 1 == 0) {
-			largestPrimeFactor = multOfSix - 1;
-			while (n % multOfSix - 1 == 0) {
-				n = n / largestPrimeFactor;
-			}
+		while (n % possFactor == 0) {
+			n = n / largestPrimeFactor;
 		}
+		possFactor += 2;
+		std::cout << largestPrimeFactor << std::endl;
 	}
 
+	if (largestPrimeFactor < n) {
+		largestPrimeFactor = n;
+	}
+
+	std::cout << "Largest prime factor: " << largestPrimeFactor << std::endl;
 }
